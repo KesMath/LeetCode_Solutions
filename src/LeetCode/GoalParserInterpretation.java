d1e3e8f5fd34d7de25608e2f668761dc5a4d4158
unknown <kesmathieu30@gmail.com>
2020-12-07 18:35:44 -0500

    adding soln for goal parser

---
+++ src/LeetCode/GoalParserInterpretation.java
@@ -0,0 +1,51 @@
+package LeetCode;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class GoalParserInterpretation {
+    private HashMap<String,String> map;
+    public GoalParserInterpretation(){
+        this.map = new HashMap<>();
+        this.map.put("G","G");
+        this.map.put("()", "o");
+        this.map.put("(al)","al");
+    }
+
+    public String interpret(String command) {
+        char[] charArr = command.toCharArray();
+        StringBuilder s = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        for(int i = charArr.length - 1; i >=0; i--){
+            stack.push(charArr[i]);
+        }
+        while(!stack.isEmpty()){
+            if(this.map.containsKey(Character.toString(stack.peek()))){
+                s.append(stack.pop());
+            }
+            else{
+                StringBuilder temp = new StringBuilder();
+                if(stack.peek() == '('){
+                    int distance = stack.search(')');
+                    for(int i = 0; i < distance; i++){
+                        temp.append(stack.pop());
+                    }
+                    s.append(this.map.get(temp.toString()));
+                }
+            }
+        }
+        return s.toString();
+    }
+
+    public static void main(String[] args) {
+        GoalParserInterpretation g = new GoalParserInterpretation();
+        System.out.println(g.interpret("(al)G(al)()()G"));
+//        Stack<Integer> s = new Stack<>();
+//        s.add(1);
+//        s.add(2);
+//        s.add(3);
+//        s.add(4);
+//        s.add(5);
+//        System.out.println(s.search(4));
+    }
+}
