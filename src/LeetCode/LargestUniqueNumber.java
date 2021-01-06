d2d49735397932381df3d963256ffc0ebf40041a
unknown <kesmathieu30@gmail.com>
2021-01-05 20:58:07 -0500

    adding another soln for largestUnique

--- src/LeetCode/LargestUniqueNumber.java
+++ src/LeetCode/LargestUniqueNumber.java
@@ -1,5 +1,6 @@
 package LeetCode;
 
+import java.util.Arrays;
 import java.util.TreeMap;
 
 public class LargestUniqueNumber {
@@ -28,8 +29,26 @@ public class LargestUniqueNumber {
         return largestUniqueNumber;
     }
 
+    public static int largestUniqueNumber1(int[] A) {
+        //given A[i] <=1000 contraint,
+        //thus freqArr[A[i]] == freqArr[1001] (due to zero based start index of arrays]
+        int[] freqArr = new int[1001];
+        int largestUniqueNum = -1;
+        for(int i = 0; i < A.length; i++){ freqArr[A[i]]++;}
+        for(int i = freqArr.length-1; i >=0; i--){
+            if(freqArr[i]==1){
+                largestUniqueNum = i;
+                break;
+            }
+        }
+        return largestUniqueNum;
+    }
+
     public static void main(String[] args) {
         System.out.println(LargestUniqueNumber.largestUniqueNumber(new int[]{9,9,8,8}));
         System.out.println(LargestUniqueNumber.largestUniqueNumber(new int[]{5,7,3,9,4,9,8,3,1}));
+        System.out.println("========================================");
+        System.out.println(LargestUniqueNumber.largestUniqueNumber1(new int[]{9,9,8,8}));
+        System.out.println(LargestUniqueNumber.largestUniqueNumber1(new int[]{5,7,3,9,4,9,8,3,1}));
     }
 }
