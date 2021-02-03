48f9c48b7c25723b53e038d8210d63fd5e82b88b
unknown <kesmathieu30@gmail.com>
2021-02-02 21:01:16 -0500

    refactoring code to remove redundant put calls

--- src/LeetCode/DecryptStringFromAlphabetToIntegerMapping.java
+++ src/LeetCode/DecryptStringFromAlphabetToIntegerMapping.java
@@ -14,32 +14,11 @@ public class DecryptStringFromAlphabetToIntegerMapping {
         StringBuilder stringBuilder = new StringBuilder();
         StringBuilder tempbuilder = new StringBuilder();
         HashMap<String, Character> map = new HashMap<>();
-        map.put("1", 'a');
-        map.put("2", 'b');
-        map.put("3", 'c');
-        map.put("4", 'd');
-        map.put("5", 'e');
-        map.put("6", 'f');
-        map.put("7", 'g');
-        map.put("8", 'h');
-        map.put("9", 'i');
-        map.put("10", 'j');
-        map.put("11", 'k');
-        map.put("12", 'l');
-        map.put("13", 'm');
-        map.put("14", 'n');
-        map.put("15", 'o');
-        map.put("16", 'p');
-        map.put("17", 'q');
-        map.put("18", 'r');
-        map.put("19", 's');
-        map.put("20", 't');
-        map.put("21", 'u');
-        map.put("22", 'v');
-        map.put("23", 'w');
-        map.put("24", 'x');
-        map.put("25", 'y');
-        map.put("26", 'z');
+        char initChar = 'a';
+        for(int i = 1; i < 27; i++){
+            map.put(Integer.toString(i), (char)(initChar));
+            initChar+=1;
+        }
 
         for(int i = arr.length-1; i >=0 ;i--){
             if(arr[i] == '#'){
