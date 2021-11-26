d873311b984ab2c83635a9aa21541638ed266b97
unknown <kesmathieu30@gmail.com>
2021-11-26 03:25:59 -0500

    adding template

---
+++ src/LeetCode/ReplaceElementsWithGreatestElementOnRightSide.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+/**
+ * DESCRIPTION: https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+ */
+public class ReplaceElementsWithGreatestElementOnRightSide {
+
+    public static int[] replaceElements(int[] arr) {
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(
+                           ReplaceElementsWithGreatestElementOnRightSide.replaceElements(
+                                   new int[]{17,18,5,4,6,1}
+                                   )
+                           )
+        );
+    }
+}
